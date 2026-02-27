1f1acff845987af6f5a86a298b981bf658bef041
Daniel Araújo <danielaraujo@acsapps.com.br>
2026-02-27 18:08:40 -0300

    Seção 5: Flutter UI: Tela home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:greengrocer/src/auth/sign_in_screen.dart';
+import 'package:greengrocer/src/pages/auth/sign_in_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.white.withAlpha(190),
+      ),
       debugShowCheckedModeBanner: false,
       home: const SignInScreen(),
     );
