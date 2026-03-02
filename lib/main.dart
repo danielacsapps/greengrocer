895ca9f62d4d574f0d3047fb1e761385c0637594
Daniel Araújo <danielaraujo@acsapps.com.br>
2026-03-02 10:31:50 -0300

    Seção 6: Flutter UI: Tela de detalhes do produto

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: Colors.white.withAlpha(190),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(foregroundColor: Colors.green),
+        ),
       ),
       debugShowCheckedModeBanner: false,
       home: const SignInScreen(),
